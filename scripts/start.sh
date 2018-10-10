22e80a7b4ddacbc619ccaac13aad3d5d40a0128e
Brian Kocoloski <brian.kocoloski@wustl.edu>
2018-10-10 17:18:46 -0500

    update start script to not mount ssd volume if not ssd is present

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,11 +3,15 @@
 TB_DIR="${HOME}/tailbench-docker"
 C_HOME="/home/cc"
 
-
-docker rm -f tailbench
-docker run --net host --name tailbench \
-    -v ${TB_DIR}/tailbench/tailbench.inputs:${C_HOME}/data \
-    -v ${TB_DIR}/tailbench/tailbench-v0.9:${C_HOME}/src \
-    -v ${TB_DIR}/results:${C_HOME}/results \
-    -v /ssd-sde/briankoco/tb-scratch:${C_HOME}/scratch \
-    -d tailbench
+if [ -d /ssd-sde/briankoco/tb-scratch ]; then
+    docker run --net host --name tailbench \
+        -v ${TB_DIR}/tailbench/tailbench.inputs:${C_HOME}/data \
+        -v ${TB_DIR}/tailbench/tailbench-v0.9:${C_HOME}/src \
+        -v /ssd-sde/briankoco/tb-scratch:${C_HOME}/scratch \
+        -d tailbench
+else
+    docker run --net host --name tailbench \
+        -v ${TB_DIR}/tailbench/tailbench.inputs:${C_HOME}/data \
+        -v ${TB_DIR}/tailbench/tailbench-v0.9:${C_HOME}/src \
+        -d tailbench
+fi
