030c5f0176da04b3af34b0bde2d1a8015e3a190b
Brian Kocoloski <brian.kocoloski@wustl.edu>
2019-01-21 00:46:46 +0000

    update untar setup command

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 wget http://tailbench.csail.mit.edu/tailbench.inputs.tgz -O tailbench/tailbench.inputs.tgz
-tar xvf tailbench/tailbench.inputs.tgz
+
+pushd tailbench
+tar xvf tailbench.inputs.tgz
+popd
